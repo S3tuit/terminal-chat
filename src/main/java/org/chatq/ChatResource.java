31a171473991f3933072790dd1d2f3f226041ca4
Matteo Ladislai <maladislai@paa.it>
2025-02-01 16:39:21 +0100

    env + basic style

--- src/main/java/org/chatq/ChatResource.java
+++ src/main/java/org/chatq/ChatResource.java
@@ -6,7 +6,7 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 
 @Path("/hello")
-public class GreetingResource {
+public class ChatResource {
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
