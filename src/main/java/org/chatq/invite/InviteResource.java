d69ff9671de3481d6f3ca6b3348969e6c87d9612
Matteo Ladislai <maladislai@paa.it>
2025-02-10 21:59:43 +0100

    Added invitation handling

--- src/main/java/org/chatq/invite/InviteResource.java
+++ src/main/java/org/chatq/invite/InviteResource.java
@@ -8,6 +8,7 @@ import jakarta.ws.rs.core.Context;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import jakarta.ws.rs.core.SecurityContext;
+import org.chatq.auth.AuthService;
 import org.chatq.chat.Chat;
 import org.chatq.users.User;
 
@@ -40,6 +41,28 @@ public class InviteResource {
             return Response.status(Response.Status.CREATED).entity(invite).build();
         } else {
             return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+    }
+
+    @POST
+    @Path("/invite-user")
+    @RolesAllowed({"User"})
+    public Response inviteUser(@QueryParam("inviteCode") String inviteCode, @Context SecurityContext ctx) {
+        // Check for token validity
+        String userId = AuthService.getClaimFromCtx(ctx, "userId");
+        if (userId == null) {
+            return Response.status(Response.Status.UNAUTHORIZED).build();
+        }
+
+        // Check for request validity
+        if (inviteCode == null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("Trying to invite with a null inviteCode?").build();
+        }
+
+        if (inviteService.inviteUserToChat(inviteCode, userId)) {
+            return Response.status(Response.Status.CREATED).build();
+        } else {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity("Oops... user did not get invited to the chat").build();
         }
     }
 }
