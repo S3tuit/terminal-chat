f8a8c20b2f4947ea0fe0afb401e0d40d5333c66a
Matteo Ladislai <maladislai@paa.it>
2025-02-22 20:20:20 +0100

    Implemented reactive redis

---
+++ src/main/java/org/chatq/connection/ChatConnectionsMap.java
@@ -0,0 +1,37 @@
+package org.chatq.connection;
+
+import io.quarkus.redis.datasource.ReactiveRedisDataSource;
+import io.quarkus.redis.datasource.set.ReactiveSetCommands;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
+
+import java.util.Set;
+
+// Maps the chatId with the current available connections that have access to that chat, chatId: Set<connectionId>
+
+@ApplicationScoped
+public class ChatConnectionRepository {
+
+    private final ReactiveSetCommands<String, String> setCommands;
+
+    public ChatConnectionRepository(ReactiveRedisDataSource reactive) {
+        this.setCommands = reactive.set(String.class);
+    }
+
+    // Add the connection to the available connections for that chatId
+    public Uni<Integer> storeAvailableConnectionForChat(ObjectId chatId, String connectionId) {
+        return setCommands.sadd("chat:" + chatId.toString(),
+                connectionId);
+    }
+
+    // Retrieve the available connections for that chat
+    public Uni<Set<String>> getAvailableConnectionsForChat(ObjectId chatId) {
+        return setCommands.smembers("chat:" + chatId.toString());
+    }
+
+    public Uni<Integer> removerConnectionFromChat(ObjectId chatId, String connectionId) {
+        return setCommands.srem("chat:" + chatId.toString(),
+                connectionId);
+    }
+}
---
+++ src/main/java/org/chatq/connection/ConnectionUserMap.java
@@ -0,0 +1,49 @@
+package org.chatq.connection;
+
+import io.quarkus.redis.datasource.ReactiveRedisDataSource;
+import io.quarkus.redis.datasource.hash.ReactiveHashCommands;
+import io.smallrye.mutiny.Uni;
+import jakarta.enterprise.context.ApplicationScoped;
+import org.bson.types.ObjectId;
+
+import java.util.Map;
+import java.util.Set;
+
+// Map the connectionId with the username and the chatIds the user has access to
+
+@ApplicationScoped
+public class ConnectionRepository {
+
+    private final ReactiveHashCommands<String, String, String> hashCommands;
+
+    public ConnectionRepository(ReactiveRedisDataSource reactive) {
+        this.hashCommands = reactive.hash(String.class);
+    }
+
+    // Store a connection, return a Long indicating the number of fields that were added to the hash
+    public Uni<Long> storeConnection(String connectionId, String username, Set<ObjectId> chatIds) {
+        String key = "connection:" + connectionId;
+        return hashCommands.hset(key, Map.of(
+                "username", username,
+                "chatIds", chatIds.toString()
+        ));
+    }
+
+    // Retrieve a connection
+    public Uni<Map<String, String>> getConnection(String connectionId){
+        String key = "connection:" + connectionId;
+        return hashCommands.hgetall(key);
+    }
+
+    // Retrieve a value from a connection
+    public Uni<String> getValueFromConnection(String connectionId, String value) {
+        String key = "connection:" + connectionId;
+        return hashCommands.hget(key, value);
+    }
+
+    // Delete a connection
+    public Uni<Integer> removeConnection(String connectionId){
+        String key = "connection:" + connectionId;
+        return hashCommands.hdel(key, new String[]{"username", "chatIds"});
+    }
+}
